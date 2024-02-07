de3f95911792827fbdb3bf062cde9e64d6fb7cfc
kouski <72550619+kouski@users.noreply.github.com>
2024-02-07 21:08:47 +0100

    organizacion listado sidebar

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class SearchBoxComponent {
 
+  @ViewChild('txtinput')
+  taginput!:ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService:GifsService) {
+
+  }
+
+  searchTag(){
+    const newTag = this.taginput.nativeElement.value;
+
+    this.gifsService.searchTag(newTag);
+
+    this.taginput.nativeElement.value='';
+  }
 }
