8655ba144921808211c69ddc40ce7b3479d1adbd
kouski <72550619+kouski@users.noreply.github.com>
2024-02-07 13:29:49 +0100

    incluir card list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
